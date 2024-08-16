5f7ad2c0ee88ad215409bd437c7aea423f242d1a
malumarcondes16 <129201647+malumarcondes16@users.noreply.github.com>
2024-08-16 13:20:25 -0300

    Update app.py

--- LIVROS/app.py
+++ LIVROS/app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 CORS(app)
 
-gemini.configure(api_key="AIzaSyBXv99VojkeqKASrRl_67TBHJhQdZecPMM")
+gemini.configure(api_key="INSIRA A CHAVE DA SUA API AQUI")
 
 model = gemini.GenerativeModel('gemini-1.5-flash')
 
